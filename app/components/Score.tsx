55ac0f569445ca86ec02d8be6742b4c608866aa4
Ethan Frot <contact@ethanfrot.com>
2025-10-14 09:40:50 +0200

    🚚 Move and rename score component

--- app/components/Score.tsx
+++ app/components/Score.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function ScorePage() {
+export default function Score() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [pseudo, setPseudo] = useState('');
